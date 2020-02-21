994194df4a059328d5b93d8e0a7c0155ebd2dc43
Maureen Josephine <Maureen Josephine>
2020-02-21 20:16:45 +0300

    initial commit

---
+++ src/main/java/com/example/csv_generation_java/CsvGenerationJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.csv_generation_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvGenerationJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvGenerationJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/csv_generation_java/CsvGenerationJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.csv_generation_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvGenerationJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
