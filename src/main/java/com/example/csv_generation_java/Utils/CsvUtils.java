3c7304316793cceb495d232af47ba0d2334bb23e
Maureen Josephine <Maureen Josephine>
2020-02-23 00:06:50 +0300

    add list of developers

--- src/main/java/com/example/csv_generation_java/Utils/CsvUtils.java
+++ src/main/java/com/example/csv_generation_java/Utils/CsvUtils.java
@@ -1,4 +1,24 @@
 package com.example.csv_generation_java.Utils;
 
+
+import java.io.FileWriter;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+import com.example.csv_generation_java.domain.Developer;
+
 public class CsvUtils {
+    public static void main(String[] args) throws Exception{
+        String csvFile = "home/Documents/developer.csv";
+        FileWriter writer = new FileWriter(csvFile);
+
+        List<Developer> developers = Arrays.asList(
+                new Developer("jos", new BigDecimal(10000), 25),
+                new Developer("Maureen", new BigDecimal(20000), 27),
+                new Developer("Akello", new BigDecimal(15000), 26)
+        );
+        
+    }
+
 }
