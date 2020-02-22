dcb90dfe6f837f7778c0e52de29906bd36b42ed4
Maureen Josephine <Maureen Josephine>
2020-02-22 23:43:35 +0300

    initilize developer variables

--- src/main/java/com/example/csv_generation_java/domain/Developer.java
+++ src/main/java/com/example/csv_generation_java/domain/Developer.java
@@ -1,4 +1,17 @@
 package com.example.csv_generation_java.domain;
 
+import java.math.BigDecimal;
+
 public class Developer {
+
+    private String name;
+    private BigDecimal salary;
+    private int age;
+
+    public Developer(String name, BigDecimal salary, int age){
+        this.name= name;
+        this.salary= salary;
+        this.age= age;
+
+    }
 }
